7ecaa48335d2f717985f65becfcb0c95e6c6404f
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-08 13:34:48 +0900

    read csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path("loan_history.csv")?;
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
